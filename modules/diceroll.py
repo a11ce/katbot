7c65a36bdb2720bf7369378d9ad328e313f6e2e2
a11ce <a11ce@protonmail.com>
2021-04-02 17:52:05 -0500

    Add possible opposition modifier for Fate

--- modules/diceroll.py
+++ modules/diceroll.py
@@ -12,26 +12,28 @@ fudge_words = [
 
 
 def respondOnText(messageText, messageData):
-    # matches: number of dice, dice size or F, addition with sign
-    if (matched := re.findall("r([0-9]+)d([0-9]+|F)((?:\+|\-)[0-9]+)?",
-                              messageText)):
+    # matches: number of dice, dice size or F, addition with sign, possible opposition modifier
+    if (matched :=
+            re.findall("r([0-9]+)d([0-9]+|F)((?:\+|\-)[0-9]+)?(\-[0-9+])?",
+                       messageText)):
         matched = matched[0]
         if matched[1] == "F":
             diceRolled = [
                 random.randint(-1, 1) for _ in range(int(matched[0]))
             ]
             rolledSum = sum(diceRolled)
-            fudge_offset = sum(diceRolled) + (int(matched[2])
-                                              if matched[2] else 0)
+            fudge_offset = sum(diceRolled) + (int(matched[2]) if matched[2]
+                                              else 0) + (int(matched[3])
+                                                         if matched[3] else 0)
             if fudge_offset < -3:
                 word = "Sub-terrible"
             elif fudge_offset > 3:
                 word = "**LEGENDARY**"
             else:
                 word = fudge_words[fudge_offset + 3]
-            return "`{}`\n{}! ({}{})".format(
+            return "`{}`\n{}! ({}{}{})".format(
                 " ".join([["-", " ", "+"][d + 1] for d in diceRolled]), word,
-                sum(diceRolled), matched[2])
+                sum(diceRolled), matched[2], matched[3])
 
         else:
             diceRolled = [
@@ -42,4 +44,3 @@ def respondOnText(messageText, messageData):
             return "`{}`\n**{}** ({}{})".format(" ".join(map(str, diceRolled)),
                                                 total, sum(diceRolled),
                                                 matched[2])
-    return False
