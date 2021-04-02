4167a0839dec824b2e0763e8131282f62f1337ba
a11ce <a11ce@protonmail.com>
2021-04-02 04:46:39 -0500

    Add animal facts module

---
+++ modules/animalFacts.py
@@ -0,0 +1,36 @@
+import requests
+
+INFO = {'name': 'animal facts', 'desc': ':)'}
+
+animals = {
+    'lizard': 'lizards',
+    'raccoon': 'raccoon',
+    'geese': 'goose',
+    'sheep': 'sheep',
+    'dingo:': 'dingo',
+    'camel': 'camel',
+    'crab': 'crab',
+    'cow': 'cow',
+    'hippo': 'hippo',
+    'fish': 'fish',
+    'chicken': 'chicken',
+    'shark': 'shark',
+    'lion': 'lion',
+    'whale': 'whale',
+    'snake': 'snake',
+    'cat': 'cat',
+    'dog': 'dog',
+    'bear': 'bear',
+    'orca': 'killerwhale',
+    'giraffe': 'giraffe',
+    'dolphin': 'dolphin',
+}
+
+baseURL = "https://and-here-is-my-code.glitch.me/facts/"
+
+
+def respondOnText(messageText):
+    for animal, endpoint in animals.items():
+        if animal in messageText.lower():
+            return list(requests.get(baseURL + endpoint).json().values())[0]
+    return False
