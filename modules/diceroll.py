f7e83ed9bdc08301b2f8aeab9adece543c1b1cdf
a11ce <a11ce@protonmail.com>
2021-04-02 05:16:21 -0500

    Add fudge dice

--- modules/diceroll.py
+++ modules/diceroll.py
@@ -1,7 +1,11 @@
 import re
 import random
 
-INFO = {'name': 'diceroll', 'desc': 'runs on NdM'}
+INFO = {'name': 'diceroll', 'desc': 'rolls on NdM or fudge on NdF'}
+
+fudge_words = [
+    "Terrible", "Poor", "Mediocre", "Fair", "Good", "Great", "Superb"
+]
 
 
 def respondOnText(messageText):
@@ -10,4 +14,10 @@ def respondOnText(messageText):
         diceRolled = [random.randint(1, found[1]) for _ in range(found[0])]
         return "Rolled {} for a total of {}".format(diceRolled,
                                                     sum(diceRolled))
+    elif (n := re.findall("([0-9]+)dF", messageText)):
+        n = int(n[0])
+        diceRolled = [random.randint(-1, 1) for _ in range(n)]
+        word = fudge_words[sum(diceRolled) + 3]
+        return "`{}`\n{}!".format(
+            " ".join([["-", " ", "+"][d + 1] for d in diceRolled]), word)
     return False
