aba9c3a7c9c6cd07cf6ed6aa09f99f6c9c406d0c
a11ce <a11ce@protonmail.com>
2021-04-02 12:24:57 -0500

    Basic polite responses

---
+++ modules/politeness.py
@@ -0,0 +1,24 @@
+import random
+
+INFO = {
+    'name': 'politeness',
+    'desc': 'responds to certain phrases when mentioned'
+}
+
+greetings = ["hi", "hello", "hey"]
+thankYous = ["thanks", "ty", "thank you"]
+youreWelcomes = ["you're welcome", "ofc", "no prob", "np", "yw", "any time"]
+
+# this is a weird model of human interaction huh
+mappings = [(greetings, greetings), (thankYous, youreWelcomes)]
+
+
+def respondOnText(messageText, messageData):
+    messageText = messageText.lower()
+    if "katbot" in messageText and "tell" not in messageText:
+        for triggers, responses in mappings:
+            if any(trigger in messageText for trigger in triggers):
+                return "{} <@{}>!".format(random.choice(responses),
+                                          messageData['sender'].id)
+
+    return False
