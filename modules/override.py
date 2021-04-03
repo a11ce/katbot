19fa854f7b4e578d1182cd1fcab0f5bc5593ea02
a11ce <a11ce@protonmail.com>
2021-04-03 14:15:10 -0500

    Add PID in admin

--- modules/override.py
+++ modules/override.py
@@ -1,11 +1,15 @@
 import socket
+import os
 
 INFO = {'name': 'admin override', 'desc': 'only works when called by a11ce'}
 
 
 def respondOnText(messageText, messageData):
     if messageData['sender'].id == 298235229095723008:
-        if messageText.startswith("katbot override sigkill"):
-            exit()
-        if messageText.startswith("katbot override whereareyou"):
-            return socket.gethostname()
+        if messageText.startswith("katbot override"):
+            if "sigkill" in messageText:
+                exit()
+            if "whereareyou" in messageText:
+                return socket.gethostname()
+            if "pid" in messageText:
+                return os.getpid()
