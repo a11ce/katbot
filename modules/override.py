39a9d28e2d94d53f1eaed5daf2cbd1f277513711
a11ce <a11ce@protonmail.com>
2021-04-03 01:29:59 -0500

    Admin location display

--- modules/override.py
+++ modules/override.py
@@ -1,3 +1,5 @@
+import socket
+
 INFO = {'name': 'admin override', 'desc': 'only works when called by a11ce'}
 
 
@@ -5,3 +7,5 @@ def respondOnText(messageText, messageData):
     if messageData['sender'].id == 298235229095723008:
         if messageText.startswith("katbot override sigkill"):
             exit()
+        if messageText.startswith("katbot override location"):
+            return socket.gethostname()
