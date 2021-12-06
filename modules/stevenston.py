a538a9956e4076e5db6a4fb346184cf5c893ff1c
a11ce <a11ce@protonmail.com>
2021-12-06 02:30:29 -0600

    Better reacts

--- modules/stevenston.py
+++ modules/stevenston.py
@@ -20,9 +20,18 @@ async def respondOnText(messageText, messageData):
             return [("<@396422982857392139> you're addicted to bedwars",
                      random.randint(10, 30))]
 
-        # dilan. also reaction test
-        if sender == 203483343281455104 and (not random.randint(0, 50)):
-            return {'reacts': ["🇨", "🇷", "🇮", "🇳", "🇬", "🇪"]}
+        if (not random.randint(0, 100)):
+            return {
+                'reacts':
+                random.choice([["🇨", "🇷", "🇮", "🇳", "🇬", "🇪"],
+                               [
+                                   "🇧",
+                                   "🇦",
+                                   "🇸",
+                                   "🇪",
+                                   "🇩",
+                               ], ["⭐"]])
+            }
 
         # gamerbot
         if sender == 813921893567692820:
