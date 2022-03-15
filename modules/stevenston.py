4841f7110866217f2ac25065a05cc7e3ffd296d4
a11ce <a11ce@protonmail.com>
2022-03-15 06:11:48 -0500

    More stevenston memes

--- modules/stevenston.py
+++ modules/stevenston.py
@@ -1,7 +1,8 @@
 import random
+import spacy
+import pyinflect
 
 INFO = {'name': 'stevenston', 'desc': "interactions for katbot's home server"}
-
 
 #async def respondOnDelete(message):
 #    if message.guild.id == 706345833750069260:
@@ -11,27 +12,54 @@ INFO = {'name': 'stevenston', 'desc': "interactions for katbot's home server"}
 #            return "dilan moment (meta):\n> {}".format(
 #                message.content.split(">")[-1])
 #
+
+nlp = spacy.load('en_core_web_sm')
+
+
 async def respondOnText(messageText, messageData):
     if messageData['message'].guild.id == 706345833750069260:
         sender = messageData['sender'].id
+
+        if messageData['message'].channel.id == 950126167493595206:
+            await messageData['message'].delete()
+
+        if ("katbot ping") in messageText.lower():
+            pstr = ""
+            for member in messageData['message'].guild.members:
+                pstr += "<@{}> ".format(member.id)
+            return pstr
+
+        #print(messageText)
+        if (not random.randint(0, 20)):
+            tokens = nlp(messageText)
+            for token in tokens:
+                if token.pos_ == 'VERB':
+                    for item in token.children:
+                        if (item.dep_ == "dobj" or item.dep_ == "dative"):
+                            vp = str(token._.inflect("VBD"))
+                            if vp == "None":
+                                vp = str(token) + "ed"
+                        #print(vp)
+                            return "i " + vp + " your mom's " + str(
+                                item) + " last night"
 
         # claire
         if sender == 396422982857392139 and messageText.startswith("s!bw"):
             return [("<@396422982857392139> you're addicted to bedwars",
                      random.randint(10, 30))]
 
-        if (not random.randint(0, 100)):
-            return {
-                'reacts':
-                random.choice([["🇨", "🇷", "🇮", "🇳", "🇬", "🇪"],
-                               [
-                                   "🇧",
-                                   "🇦",
-                                   "🇸",
-                                   "🇪",
-                                   "🇩",
-                               ], ["⭐"]])
-            }
+        #if (not random.randint(0, 100)):
+        #    return {
+        #        'reacts':
+        #        random.choice([["🇨", "🇷", "🇮", "🇳", "🇬", "🇪"],
+        #                       [
+        #                           "🇧",
+        #                           "🇦",
+        #                           "🇸",
+        #                           "🇪",
+        #                           "🇩",
+        #                       ], ["⭐"]])
+        #    }
 
         # gamerbot
         if sender == 813921893567692820:
